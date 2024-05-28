cc6551f01986b616f9c5c6b4a4778922f7c2a6af
João Moniz <primed.5353@gmail.com>
2024-05-28 14:35:22 +0100

    Map controller removed and crude routing added

--- Code/site/create_route.js
+++ Code/site/create_route.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let map;
+    let markers = [];
+    let selectedWaypoints = [];
+    let directionsService;
+    let directionsRenderer;
+
     function initMap() {
-        var location = { lat: 38.72570143190969, lng: -9.149985452443389 };
-        var map = new google.maps.Map(document.getElementById('map'), {
+        const location = { lat: 38.72570143190969, lng: -9.149985452443389 };
+        map = new google.maps.Map(document.getElementById('map'), {
             zoom: 13,
             center: location,
-            styles:[
+            styles: [
+
                 {
                     featureType: 'poi',
                     stylers: [{ visibility: 'off' }]
@@ -128,19 +135,71 @@ document.addEventListener('DOMContentLoaded', () => {
             ]
         });
 
+        directionsService = new google.maps.DirectionsService();
+        directionsRenderer = new google.maps.DirectionsRenderer();
+        directionsRenderer.setMap(map);
+
         fetch('http://localhost:3000/lqrcodes')
-        .then(response => response.json())
-        .then(lqrcodes => {
-            lqrcodes.forEach(qrcode => {
-                var marker = new google.maps.Marker({
-                    position: { lat: qrcode.lqrcode_latitude, lng: qrcode.lqrcode_longitude },
-                    map: map,
-                    title: `QR Code at ${qrcode.lqrcode_latitude}, ${qrcode.lqrcode_longitude}`
+            .then(response => response.json())
+            .then(lqrcodes => {
+                lqrcodes.forEach(qrcode => {
+                    const marker = new google.maps.Marker({
+                        position: { lat: qrcode.lqrcode_latitude, lng: qrcode.lqrcode_longitude },
+                        map: map,
+                        title: `QR Code at ${qrcode.lqrcode_latitude}, ${qrcode.lqrcode_longitude}`
+                    });
+
+                    marker.addListener('click', () => {
+                        handleWaypointSelection(marker);
+                    });
+
+                    markers.push(marker);
                 });
-            });
-        })
-        .catch(error => console.error('Erro ao buscar QR codes:', error));
-}
+            })
+            .catch(error => console.error('Erro ao buscar QR codes:', error));
+    }
+
+    function handleWaypointSelection(marker) {
+        const markerIndex = selectedWaypoints.indexOf(marker);
+        if (markerIndex === -1) {
+            if (selectedWaypoints.length < 2) {
+                selectedWaypoints.push(marker);
+                marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png'); // change color of selected marker
+            }
+        } else {
+            selectedWaypoints.splice(markerIndex, 1);
+            marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png'); // reset color of deselected marker
+        }
+
+        if (selectedWaypoints.length === 2) {
+            calculateAndDisplayRoute();
+        }
+    }
+
+    function calculateAndDisplayRoute() {
+        const waypoints = selectedWaypoints.map(marker => ({
+            location: marker.getPosition(),
+            stopover: true,
+        }));
+
+        directionsService.route(
+            {
+                origin: waypoints[0].location,
+                destination: waypoints[1].location,
+                travelMode: google.maps.TravelMode.DRIVING,
+            },
+            (response, status) => {
+                if (status === 'OK') {
+                    directionsRenderer.setDirections(response);
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            }
+        );
+
+        selectedWaypoints.forEach(marker => marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png'));
+        selectedWaypoints = [];
+    }
 
     window.initMap = initMap;
 });
