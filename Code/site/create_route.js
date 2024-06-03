3a9c0bb3187bba3b0f8dab60e46928d71da8e167
João Moniz <primed.5353@gmail.com>
2024-06-03 20:53:49 +0100

    map fixed

--- Code/site/create_route.js
+++ Code/site/create_route.js
@@ -1,208 +1,104 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let map;
-    let markers = [];
-    let selectedWaypoints = [];
-    let directionsService;
-    let directionsRenderer;
+let map;
+let markers = [];
+let selectedWaypoints = [];
+let directionsService;
+let directionsRenderer;
 
-    function initMap() {
-        const location = { lat: 38.72570143190969, lng: -9.149985452443389 };
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 13,
-            center: location,
-            mapTypeControl: false, // Remove map and satellite buttons
-            streetViewControl: false, // Remove street view button
-            fullscreenControl: false, // Remove fullscreen button
-            styles: [
-                {
-                    featureType: 'poi',
-                    stylers: [{ visibility: 'off' }]
-                },
-                {
-                    featureType: 'poi.business',
-                    elementType: 'labels.icon',
-                    stylers: [{ visibility: 'off' }]
-                }, 
-                {
-                    "featureType": "building",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#343434"
-                        }
-                    ]
-                }, {
-                    "featureType": "road",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.highway",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.arterial",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.local",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#757575"
-                        }
-                    ]
-                }, {
-                    "featureType": "water",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#030303"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape",
-                    "stylers": [
-                        {
-                            "color": "#161616"
-                        }
-                    ]
-                },
-                {
-                    featureType: 'transit',
-                    stylers: [{ visibility: 'off' }]
-                },
-                {
-                    "elementType": "labels.text",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#000000"
-                        },
-                        {
-                            "weight": 0.6
-                        },
-                        {
-                            "fontSize": 6
-                        }
-                    ]
-                }, {
-                    "elementType": "labels.text.stroke",
-                    "stylers": [
-                        {
-                            "color": "#FFFFFF"
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                }, {
-                    "elementType": "labels",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "labels.text",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                }
-            ]
-        });
+function initMap() {
+    const location = { lat: 38.72570143190969, lng: -9.149985452443389 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: location,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false,
+        styles: [
+            { "featureType": "poi", "stylers": [{ "visibility": "off" }] },
+            { "featureType": "poi.business", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] },
+            { "featureType": "building", "elementType": "geometry", "stylers": [{ "color": "#343434" }] },
+            { "featureType": "road", "elementType": "geometry", "stylers": [{ "color": "#757575" }] },
+            { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#757575" }] },
+            { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#757575" }] },
+            { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#757575" }] },
+            { "featureType": "water", "elementType": "geometry.fill", "stylers": [{ "color": "#030303" }] },
+            { "featureType": "landscape", "stylers": [{ "color": "#161616" }] },
+            { "featureType": "transit", "stylers": [{ "visibility": "off" }] },
+            { "elementType": "labels.text", "stylers": [{ "visibility": "on" }, { "color": "#000000" }, { "weight": 0.6 }, { "fontSize": 6 }] },
+            { "elementType": "labels.text.stroke", "stylers": [{ "color": "#FFFFFF" }, { "visibility": "on" }] },
+            { "elementType": "labels", "stylers": [{ "visibility": "off" }] },
+            { "featureType": "road", "elementType": "labels.text", "stylers": [{ "visibility": "on" }] }
+        ]
+    });
 
-        directionsService = new google.maps.DirectionsService();
-        directionsRenderer = new google.maps.DirectionsRenderer();
-        directionsRenderer.setMap(map);
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
 
-        fetch('176.78.46.208/lqrcodes')
-            .then(response => response.json())
-            .then(lqrcodes => {
-                lqrcodes.forEach(qrcode => {
-                    const marker = new google.maps.Marker({
-                        position: { lat: qrcode.lqrcode_latitude, lng: qrcode.lqrcode_longitude },
-                        map: map,
-                        title: `QR Code at ${qrcode.lqrcode_latitude}, ${qrcode.lqrcode_longitude}`
-                    });
-
-                    marker.addListener('click', () => {
-                        handleWaypointSelection(marker);
-                    });
-
-                    markers.push(marker);
+    fetch('http://176.78.46.208/lqrcodes')
+        .then(response => response.json())
+        .then(lqrcodes => {
+            lqrcodes.forEach(qrcode => {
+                const marker = new google.maps.Marker({
+                    position: { lat: qrcode.lqrcode_latitude, lng: qrcode.lqrcode_longitude },
+                    map: map,
+                    title: `QR Code at ${qrcode.lqrcode_latitude}, ${qrcode.lqrcode_longitude}`
                 });
-            })
-            .catch(error => console.error('Erro ao buscar QR codes:', error));
+
+                marker.addListener('click', () => {
+                    handleWaypointSelection(marker);
+                });
+
+                markers.push(marker);
+            });
+        })
+        .catch(error => console.error('Error fetching QR codes:', error));
+}
+
+function handleWaypointSelection(marker) {
+    const markerIndex = selectedWaypoints.indexOf(marker);
+    if (markerIndex === -1) {
+        selectedWaypoints.push(marker);
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
+    } else {
+        selectedWaypoints.splice(markerIndex, 1);
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
     }
 
-    function handleWaypointSelection(marker) {
-        const markerIndex = selectedWaypoints.indexOf(marker);
-        if (markerIndex === -1) {
-            selectedWaypoints.push(marker);
-            marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png'); // change color of selected marker
-        } else {
-            selectedWaypoints.splice(markerIndex, 1);
-            marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png'); // reset color of deselected marker
-        }
+    if (selectedWaypoints.length > 1) {
+        calculateAndDisplayRoute();
+    } else {
+        directionsRenderer.setDirections({ routes: [] });
+    }
+}
 
-        if (selectedWaypoints.length > 1) {
-            calculateAndDisplayRoute();
-        } else {
-            directionsRenderer.setDirections({ routes: [] }); // Clear previous routes if less than 2 markers are selected
-        }
+function calculateAndDisplayRoute() {
+    if (selectedWaypoints.length < 2) {
+        return;
     }
 
-    function calculateAndDisplayRoute() {
-        if (selectedWaypoints.length < 2) {
-            return;
-        }
+    const waypoints = selectedWaypoints.slice(1, -1).map(marker => ({
+        location: marker.getPosition(),
+        stopover: true,
+    }));
 
-        const waypoints = selectedWaypoints.slice(1, -1).map(marker => ({
-            location: marker.getPosition(),
-            stopover: true,
-        }));
+    const origin = selectedWaypoints[0].getPosition();
+    const destination = selectedWaypoints[selectedWaypoints.length - 1].getPosition();
 
-        const origin = selectedWaypoints[0].getPosition();
-        const destination = selectedWaypoints[selectedWaypoints.length - 1].getPosition();
-
-        directionsService.route(
-            {
-                origin: origin,
-                destination: destination,
-                waypoints: waypoints,
-                travelMode: google.maps.TravelMode.DRIVING,
-            },
-            (response, status) => {
-                if (status === 'OK') {
-                    directionsRenderer.setDirections(response);
-                } else {
-                    window.alert('Directions request failed due to ' + status);
-                }
+    directionsService.route(
+        {
+            origin: origin,
+            destination: destination,
+            waypoints: waypoints,
+            travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (response, status) => {
+            if (status === 'OK') {
+                directionsRenderer.setDirections(response);
+            } else {
+                window.alert('Directions request failed due to ' + status);
             }
-        );
-    }
+        }
+    );
+}
 
-    window.initMap = initMap;   
-});
+document.addEventListener('DOMContentLoaded', initMap);
